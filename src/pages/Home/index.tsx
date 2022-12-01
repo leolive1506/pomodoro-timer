f5cdf3121db1f625da30ef0944ccbb078ffeca00
Leonardo Lopes <leonardolivelopes2@gmail.com>
2022-12-01 15:40:50 -0300

    reset form

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -25,7 +25,7 @@ const newCycleFormValidationSchema = zod.object({
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm<NewCycleFormData>({
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -35,6 +35,8 @@ export function Home() {
 
   function handleCreateNewCycle(data: NewCycleFormData) {
     console.log(data)
+    // reset volta para valores de default values
+    reset()
   }
 
   const task = watch('task')
@@ -65,7 +67,7 @@ export function Home() {
             placeholder="00"
             step={5}
             min={5}
-            // max={60}
+            max={60}
             {...register('minutesAmount', { valueAsNumber: true })}
           />
 
